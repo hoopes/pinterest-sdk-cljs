917af7a37822d58855d65f71958c4b50226e15f9
Matthew Hoopes <hoopes@rhiza.com>
2016-04-25 20:46:02 -0400

    not sure how i missed these

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject pinterest-sdk-cljs "0.1.0-SNAPSHOT"
+  :description "Pinterest javascript SDK wrapper for clojurescript"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojurescript "1.7.170"]])
---
+++ test/pinterest_sdk_cljs/core_test.clj
@@ -0,0 +1,7 @@
+(ns pinterest-sdk-cljs.core-test
+  (:require [clojure.test :refer :all]
+            [pinterest-sdk-cljs.core :refer :all]))
+
+;(deftest a-test
+  ;(testing "FIXME, I fail."
+    ;(is (= 0 1))))
